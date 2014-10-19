c039c6b834d8e01c9481a67891cb94cb35f414d2
rebeccacurran <rebeccacurran@users.noreply.github.com>
2014-10-19 18:25:33 +0100

    Update getcoffee.php

--- getcoffee.php
+++ getcoffee.php
@@ -10,13 +10,21 @@ mysqli_select_db($con,"Coffee");
 $sql="SELECT * FROM coffeeTable WHERE ID = '".$q."'";
 $result = mysqli_query($con,$sql);
 
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>CoffeeName</th>
+<th>CoffeeDesc</th>
+</tr>";
+
 while($row = mysqli_fetch_array($result)) {
-  echo "<tr class='success'>";
+  echo "<tr>";
   echo "<td>" . $row['ID'] . "</td>";
   echo "<td>" . $row['CoffeeName'] . "</td>";
   echo "<td>" . $row['CoffeeDesc'] . "</td>";
   echo "</tr>";
 }
+echo "</table>";
 
 mysqli_close($con);
 ?>
